7b8f202119068d5e1d40ade351034f5164d16b9c
jakekressley <kressleyjake@gmail.com>
2024-05-10 15:20:08 -0400

    cleaned up code and reorganized into further components

---
+++ src/types/Types.tsx
@@ -0,0 +1,13 @@
+type MovieItemProps = {
+    title: string;
+    userRating: number;
+    average: number;
+    hotness: number;
+    poster: number;
+    year: number;
+    overview: string;
+    genres: string[];
+    isDarkMode: boolean;
+};
+
+export default MovieItemProps;
